ce879b3f57843327ce6497775bec1f521caf0a8a
Alexander Kushko <kushko.alex@gmail.com>
2014-08-04 17:14:24 +0300

    -

--- js/main.js
+++ js/main.js
@@ -1,21 +1,43 @@
 $(function () {
 
-    $("#category-menu").find("li").mouseenter(function () {
-        var width = this.offsetWidth;
-        $(this).find("ul")
-            .css("left", width+20 + "px")
-            .css("display", "block")
-            .css("margin-top", + "-20px")
+    var obj = $("#category-menu");
+    var width = obj[0].offsetWidth;
+
+    $("html").click(function(){
+        $(".sub-menu").each(function(){
+            $(this).removeClass("show");
+        });
+    });
+
+
+    $(".sub-menu").each(function(){
+       $(this).css("left", width+20 + "px");
+    });
+
+    $("#category-menu").find("li.main").mouseenter(function () {
+
+        $(".sub-menu").each(function(){
+            $(this).removeClass("show")
+        });
+
+        $(this).find(".sub-menu")
+            .addClass("show")
+            //.css("margin-top", + "-20px")
         ;
-
     });
 
-    $("#category-menu").find("li").mouseleave(function () {
-        //setTimeout(function(){
-            $("#category-menu").find("li").find("ul").css("display", "none");
-        //},2000);
 
-    });
+
+
+
+
+
+//    $("#category-menu").find("li").mouseleave(function () {
+//        //setTimeout(function(){
+//            $("#category-menu").find("li").find("ul").css("display", "none");
+//        //},2000);
+//
+//    });
 
 
 });
